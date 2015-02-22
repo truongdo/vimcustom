087d2f52aaaf461896d220013b707edbcf467f24
truong-d <truongdq54@gmail.com>
2015-02-22 14:45:23 +0900

    Fix bugs

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 # Install pathogen
 echo "Install Pathogen"
 mkdir -p ~/.vim/autoload ~/.vim/syntax ~/.vim/bundle && \
-    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim || exit 1
 echo "Install vimrc"
 wget https://raw.githubusercontent.com/truongdq54/vimcustom/master/vimrc -O ~/.vimrc
 vim +BundleInstall
